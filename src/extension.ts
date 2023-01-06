f2fe2f832269be90d47726ac75cf2ceafe60ebfc
黄原寅 <3443718488@qq.com>
2023-01-06 17:41:49 +0800

    chore vscode命令

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,28 @@
-import * as vscode from "vscode";
-import { getFunctionNode } from "./main";
+import * as vscode from 'vscode'
+import { getFunctionNode } from './main'
 
 export function activate(context: vscode.ExtensionContext) {
-  vscode.commands.registerCommand("vscode-plugin-deleteFunction.helloWorld", () => {
-    vscode.window.showInformationMessage("删除成功!");
-  });
+  vscode.commands.registerCommand('vscode-extension.deleteFunction', () => {
+    vscode.window.showInformationMessage('删除成功!')
+  })
 
   // vscode 中 应该如何删除一个字符
-  const editor = vscode.window.activeTextEditor;
-  if (!editor) return;
+  const editor = vscode.window.activeTextEditor
+  if (!editor) return
   // 算法 业务逻辑
-  const code = editor.document.getText(); // 拿到光标所在的内容
-  const index = editor.document.offsetAt(editor.selection.active); // 拿到光标所在索引
+  const code = editor.document.getText() // 拿到光标所在的内容
+  const index = editor.document.offsetAt(editor.selection.active) // 拿到光标所在索引
 
-  const functionNode = getFunctionNode(code, index);
+  const functionNode = getFunctionNode(code, index)
 
-  if (!functionNode) return;
+  if (!functionNode) return
 
   // vscode UI
   editor?.edit((editBuilder) => {
-    editBuilder.delete(new vscode.Range(new vscode.Position(functionNode.start.line - 1, functionNode.start.column), new vscode.Position(functionNode.end.line - 1, functionNode.end.column)));
-  });
+    editBuilder.delete(
+      new vscode.Range(new vscode.Position(functionNode.start.line - 1, functionNode.start.column), new vscode.Position(functionNode.end.line - 1, functionNode.end.column))
+    )
+  })
 }
 
 export function deactivate() {}
